95f3dca6ef3aebaa06caee5fd7d2300309c2554c
Ganeoi <1813082543@qq.com>
2022-07-17 16:34:46 +0800

    update

---
+++ views/other/pageOne.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>pageOne页面</div>
+</template>
+<script>
+export default {
+  name: 'pageOne',
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ views/other/pageTwo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>pageTwo页面</div>
+</template>
+<script>
+export default {
+  name: 'pageTwo',
+  data () {
+    return {}
+  }
+}
+</script>
