4859180fe452af8866358a335ad0d26f7ad31c20
Ganeoi <95685942+Ganeoi@users.noreply.github.com>
2022-07-17 22:59:08 +0800

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
@@ -28,37 +27,4 @@ const router = new VueRouter({
   mode: 'history', 
   routes
 })
-
-=======
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-
-// 1.全局引入router
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    // Main作为所有页面的公共部分
-    path: '/',
-    name: 'Main',
-    // 2.按需引入路由组件
-    component: () => import('../views/Main.vue'),
-    redirect: '/login',
-    children: []
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/Login/login.vue')
-  }
-]
-
-// 3.对路由进行相关配置
-// 声明router变量 来接收router实例
-const router = new VueRouter({
-  mode: 'history', 
-  routes
-})
-
->>>>>>> ada1ce3e28e10678653f4fcdd6c8a602d691b7cc
 export default router
